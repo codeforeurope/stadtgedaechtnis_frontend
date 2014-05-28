0011f97bb962d011b33cac018c6184a50e88c964
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-05-28 16:31:48 +0200

    Add Extra Context Template Tag

--- urls.py
+++ urls.py
@@ -5,11 +5,14 @@ Created on 26.02.2014
 """
 
 from django.conf.urls import patterns, url, include
-from django.views.generic import TemplateView
-from stadtgedaechtnis_frontend.views import EntryView
+from django.conf import settings
+from stadtgedaechtnis_frontend.views import EntryView, ExtraContextTemplateView
 
 urlpatterns = patterns('',
-                       url(r'^$', TemplateView.as_view(template_name='stadtgedaechtnis/index.html')),
+                       url(r'^$', ExtraContextTemplateView.as_view(template_name='stadtgedaechtnis/index.html',
+                                                                   extra_context={
+                                                                       "GOOGLE_API_KEY": settings.GOOGLE_API_KEY
+                                                                   })),
                        url(r'^i18n/', include('django.conf.urls.i18n')),
                        url(r'^entry/(?P<pk>\d+)/$', EntryView.as_view()),
                        )
--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 __author__ = 'jpi'
 
 from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
 from stadtgedaechtnis_backend.models import Entry
 
 
@@ -8,3 +9,13 @@ class EntryView(DetailView):
 
     template_name = "stadtgedaechtnis/entry.html"
     model = Entry
+
+
+class ExtraContextTemplateView(TemplateView):
+
+    extra_context = {}
+
+    def get_context_data(self, **kwargs):
+        context = super(ExtraContextTemplateView, self).get_context_data(**kwargs)
+        context.update(self.extra_context)
+        return context
